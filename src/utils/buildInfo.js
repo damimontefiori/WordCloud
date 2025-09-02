8475ee766ecdf1a3e8ab4f6db077b1749e9690b7
Damian <damian@gmail.com>
2025-09-02 20:49:48 -0300

    chore: Update build info for production deployment v2.1.0

--- src/utils/buildInfo.js
+++ src/utils/buildInfo.js
@@ -1,10 +1,10 @@
 // Este archivo es generado automáticamente durante el build
 export const buildInfo = {
   "version": "v2.1.0",
-  "buildDate": "2025-09-02T22:42:18.032Z",
-  "buildTime": "02/09/2025, 19:42:18 GMT-3",
-  "buildTimestamp": 1756852938032,
-  "gitCommit": "865941c150fbcc8bed6e0d894b149eddd1265125",
+  "buildDate": "2025-09-02T23:48:53.348Z",
+  "buildTime": "02/09/2025, 20:48:53 GMT-3",
+  "buildTimestamp": 1756856933348,
+  "gitCommit": "bb7333f0abe790b18d550d11599cea6e8f31716d",
   "gitBranch": "main",
   "environment": "production"
 }
