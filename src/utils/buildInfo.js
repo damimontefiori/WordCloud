1c22016fdc716b6524ebc2652627d22e241ba670
Damian <damian@gmail.com>
2025-09-02 22:16:43 -0300

    Actualizar buildInfo.js

--- src/utils/buildInfo.js
+++ src/utils/buildInfo.js
@@ -1,10 +1,10 @@
 // Este archivo es generado automáticamente durante el build
 export const buildInfo = {
   "version": "v2.1.0",
-  "buildDate": "2025-09-03T00:25:43.507Z",
-  "buildTime": "02/09/2025, 21:25:43 GMT-3",
-  "buildTimestamp": 1756859143507,
-  "gitCommit": "8475ee766ecdf1a3e8ab4f6db077b1749e9690b7",
+  "buildDate": "2025-09-03T01:04:37.512Z",
+  "buildTime": "02/09/2025, 22:04:37 GMT-3",
+  "buildTimestamp": 1756861477512,
+  "gitCommit": "61565f9b5271e3afa2dea38dd7ecaf4c1f903390",
   "gitBranch": "main",
   "environment": "production"
 }
